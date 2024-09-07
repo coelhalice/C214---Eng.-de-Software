04b9b0766a0b5f6b408190698533d944d103328c
Alice de Lorenzo Coelho <alicedelorc@gmail.com>
2024-09-07 15:28:40 -0300

    feat: Adiciona função para simular obtenção de dados do servidor remoto

---
+++ src/servidorRemoto.js
@@ -0,0 +1,11 @@
+// Simula uma função que retorna os dados de um servidor remoto
+function obterDados() {
+    return {
+      nomeDoProfessor: 'Professor RenZo',
+      horarioDeAtendimento: '10:00 - 12:00',
+      periodo: 'integral',
+      sala: 3
+    };
+  }
+  
+  module.exports = { obterDados };
